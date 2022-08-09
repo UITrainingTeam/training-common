65fe7e4cac8111fd94f5ffd5a35c324c7c98d602
Long Le <levanlong.91@gmail.com>
2022-08-09 23:02:46 +0700

    Update chat event

--- src/common/enum/socket-event.enum.ts
+++ src/common/enum/socket-event.enum.ts
@@ -4,4 +4,10 @@ export enum SocketEventType {
     JOIN_ROOM = 'subscribe',
     LEAVE_ROOM = 'unsubscribe',
     USER_METADATA = 'user-metadata',
+    MESSAGE = 'message',
+    CHANNEL_READY = 'channelCreated',
+    REQUEST_JOIN_CHANNEL = 'requestJoinChannel',
+    RESPONSE_JOIN_CHANNEL = 'responseJoinChannel',
+    ADD_MEMBER = 'addMember',
+    REMOVE_MEMBER = 'removeMember',
 }
